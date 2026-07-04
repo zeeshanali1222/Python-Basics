42624fa309ee298d96789d80294a343705723c17
zeeshanali1222 <xeeshi.ali12@gmail.com>
2026-07-05 04:58:27 +0500

    Python

--- if-else.py
+++ if-else.py
@@ -114,8 +114,12 @@
 
 # -----------------------------------------------------------------------
 
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    print(f"{num} is an even number.")
-else:
-    print(f"{num} is an odd number.")
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(f"{num} is an even number.")
+# else:
+#     print(f"{num} is an odd number.")
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
