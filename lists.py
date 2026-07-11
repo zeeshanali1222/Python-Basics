14a5388ed9af288960a69310b0f0d327bcec42ec
zeeshanali1222 <xeeshi.ali12@gmail.com>
2026-07-11 16:39:51 +0500

    Python

--- lists.py
+++ lists.py
@@ -1,46 +1,46 @@
 # Practice Lists in Python
 
-names = ["Ali", "Ahmed", "Hassan", "Hussain"]
-print(names)
+# names = ["Ali", "Ahmed", "Hassan", "Hussain"]
+# print(names)
 
-fruits = []
-print(fruits)
+# fruits = []
+# print(fruits)
 
 # Adding elements to the list
 # First method to add elements to the list is using append() method
 # Append method adds the element at the end of the list
 # We can only add one value in append function
 
-fruits.append("Apple")
-print(fruits)
+# fruits.append("Apple")
+# print(fruits)
 
-fruits.append("Banana")
-print(fruits)
+# fruits.append("Banana")
+# print(fruits)
 
 # Second method to add elements to the list is using insert() method
 # Insert method adds the element at the specified index
 # We can only add one value in insert function
 
-fruits.insert(1, "Mango")
-print(fruits)
+# fruits.insert(1, "Mango")
+# print(fruits)
 
-fruits.insert(2, "Guava")
-print(fruits)
+# fruits.insert(2, "Guava")
+# print(fruits)
 
 # How can we add multiple values to the list at once?
 # We can use extend() method to add multiple values to the list at once
 
-fruits.extend(["Orange", "Pineapple", "Grapes"])
-print(fruits)
+# fruits.extend(["Orange", "Pineapple", "Grapes"])
+# print(fruits)
 
 # How can we count the specific element in the list?
 # We can use count() method to count the specific element in the list
 
-fruit_count = fruits.count("Apple")
-print(fruit_count)
+# fruit_count = fruits.count("Apple")
+# print(fruit_count)
 
-fruit_count1 = fruits.count("Banana")
-print(fruit_count1)
+# fruit_count1 = fruits.count("Banana")
+# print(fruit_count1)
 
 # Methods to copy the list
 # First method to copy the list is using copy() method
@@ -48,23 +48,34 @@ print(fruit_count1)
 # Its called Copy by Value. That means if we change the value of the copied list, 
 # it will not affect the original list
 
-fruits_copy = fruits.copy()
-print(fruits_copy)
+# fruits_copy = fruits.copy()
+# print(fruits_copy)
 
 # Second method to copy the list is simply assigning the list to a new variable
 # This method creates a reference to the original list
 # Its called Copy by Reference. That means if we change the value of the copied list,
 
-fruits_copy1 = fruits
-print(fruits_copy1)
+# fruits_copy1 = fruits
+# print(fruits_copy1)
 
 # For example, if we change the value of the copied list, it will affect the original list
 
-fruits.append("Strawberry")
-print(fruits)
+# fruits.append("Strawberry")
+# print(fruits)
 
 # Lets see if our copied list is affected or not
 
-print(f"Our Original List: {fruits_copy}")
-print(f"Reference List: {fruits_copy1}")
-print(f"Original List: {fruits}")
+# print(f"Our Original List: {fruits_copy}")
+# print(f"Reference List: {fruits_copy1}")
+# print(f"Original List: {fruits}")
+
+# ----------------------------------------------------------------------------------------
+
+num_list = [1, 2, 3, 4, 5]
+print(num_list)
+
+# Appending elements to the list
+
+num_list.append(6)
+print(num_list)
+
