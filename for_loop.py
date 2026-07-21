9b986792bbd4eee0e62b13ab34e2346a3eba76c4
zeeshanali1222 <xeeshi.ali12@gmail.com>
2026-07-21 17:33:29 +0500

    Python

--- for_loop.py
+++ for_loop.py
@@ -91,3 +91,17 @@
 #         break
 #     else:
 #         print("Invalid Number: ", x)
+
+# ---------------------------------------------------------------------------------------------------
+
+# Find the factorial of a given number
+
+x = int(input("Enter any number to find the factorial: "))
+factorial = 1
+
+if x <= 0:
+    print("Factorial does not exist for negative numbers. ")
+else:
+    for i in range(1, x + 1):
+        factorial = factorial * i
+    print(f"The factorial of {x} is {factorial}: ")
